36c5fefa9e27143407c6428b799b61e42f02f67c
Lin_Tzu_Hsiang <thlin002@gmail.com>
2025-04-05 18:55:57 +0800

    The Phonebook, step 11 & 12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,23 @@
 import { useState, useEffect } from 'react'
 import phonebook from './services/phonebook'
 
+const Message = ( {message, color} ) => {
+  const msgStyle = {
+    color: color,
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+  return (
+    <div style={msgStyle}>
+      {message}
+    </div>
+  )
+}
+
 const DeleteButton = ( {person, handleDelete} )  => {
   const onClick = () => {
     if(confirm(`Delete ${person.name} ?`)) {
@@ -54,10 +71,21 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState({ content: null, color: null })
 
   useEffect(() => {
     phonebook.getAll().then(data => setPersons(data))
   }, [])
+
+  const handleRequestError = (response, person = null) => {
+    if(response.status === 404 && person) {
+      setMessage({ content: `Information of ${person.name} has already been removed from server`, color: 'red' })
+      setPersons(persons.filter((p) => p.name !== person.name))
+    } else {
+      setMessage({ content: 'Error', color: 'red' })
+    }
+    setTimeout(() => setMessage({ content: null, color: null }), 5000)
+  }
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -74,9 +102,12 @@ const App = () => {
   const deleteContact = (id) => {
     phonebook
       .remove(id)
-      .then((response) => {
-        setPersons(persons.filter((person) => person.id != response.id))
+      .then((removedPerson) => {
+        setPersons(persons.filter((person) => person.id !== removedPerson.id))
+        setMessage({ content: `Removed ${removedPerson.name}`, color: 'green' })
+        setTimeout(() => setMessage({ content: null, color: null }), 5000)
       })
+      .catch((response) => handleRequestError(response, persons.find((person) => person.id === id)))
   }
 
   const addContact = (e) => {
@@ -90,12 +121,21 @@ const App = () => {
       if(confirm(`${newName} is already added to phonebook, replace the old number with a new one ?`)){
         phonebook
           .update(persons[indexFound].id, personObject)
-          .then(updatedPerson => setPersons(persons.map(person => person.id === persons[indexFound].id ? updatedPerson : person)))
+          .then(updatedPerson => {
+            setPersons(persons.map(person => person.id === persons[indexFound].id ? updatedPerson : person))
+            setMessage({ content: `Changed ${updatedPerson.name}'s number`, color: 'green' })
+            setTimeout(() => setMessage({ content: null, color: null }), 5000)
+          })
+          .catch((response) => handleRequestError(response, persons[indexFound]))
       }
     } else {
       phonebook
         .create(personObject)
-        .then(newPerson => setPersons(persons.concat(newPerson)))
+        .then(newPerson => {
+          setPersons(persons.concat(newPerson))
+          setMessage({ content: `Added ${newPerson.name}`, color: 'green' })
+          setTimeout(() => setMessage({ content: null, color: null }), 5000)
+        })
     }
 
     setNewName('')
@@ -107,6 +147,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      {message.content ? <Message message={message.content} color={message.color} /> : null}
       <Filter value={filter} onChange={handleFilterChange} />
       <h2>Add a new</h2>
       <PersonForm
